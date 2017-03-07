46d95828b04b47f42d88d950a85e1fa46c22d412
Md. Mahadi Hasan Nahid <nahid@student.sust.edu>
2017-03-07 18:31:09 +0600

    image read using opencv

---
+++ imageRead.py
@@ -0,0 +1,39 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Tue Mar 07 18:28:03 2017
+
+@author: NAHID
+"""
+
+import numpy as np
+import cv2
+
+#Displaying an Image using opencv
+
+#img = cv2.imread('nahid.jpg',0)
+img = cv2.imread('mdb001.pgm', 0)
+cv2.imshow('image',img)
+k = cv2.waitKey(0)
+if k == 27:         # wait for ESC key to exit
+    cv2.destroyAllWindows()
+elif k == ord('s'): # wait for 's' key to save and exit
+    cv2.imwrite('nahidgray.png',img)
+    cv2.destroyAllWindows()
+
+
+
+#Using mathplotlib
+
+
+from matplotlib import pyplot as plt
+
+img = cv2.imread('mdb001.pgm',0)
+plt.imshow(img, cmap = 'gray', interpolation = 'bicubic')
+plt.xticks([]), plt.yticks([])  # to hide tick values on X and Y axis
+plt.show()  
+
+print img.shape
+
+imgarr = np.array(img) 
+
+print imgarr
